b23d32194f1b5aedf4c76f748c08d95f8262502c
Andrew Holway <andrew.holway@gmail.com>
2021-12-25 10:39:24 +0100

    first json lesson

---
+++ 2/2.2_assign_item_to_var.py
@@ -0,0 +1,10 @@
+print("Access item in list by it's index")
+# Define our list
+mylist = [0,1,2,3,4,5,6,7,8,9]
+# get the first item in the list
+
+otherlist = mylist[3:7] # get items 3 thru 6 and assign to new variable.
+myinteger = mylist[4] # get 5th item and assign to new variable.
+
+print(otherlist)
+print(myinteger)
